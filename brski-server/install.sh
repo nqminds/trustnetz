dbd40e4ed32c5fdb4ec2a80cb083eec3401a6031
Alexandru Mereacre <mereacre@gmail.com>
2023-11-28 23:22:47 +0000

    feat: added second wlan config

--- brski-server/install.sh
+++ brski-server/install.sh
@@ -8,3 +8,11 @@ service dnsmasq stop
 systemctl stop wpa_supplicant
 systemctl disable wpa_supplicant
 
+cp wlan0.conf /etc/hostapd/wlan0.conf
+systemctl enable hostapd@wlan0.service 
+systemctl start hostapd@wlan0.service
+
+cp wlan1.conf /etc/hostapd/wlan1.conf
+systemctl enable hostapd@wlan1.service 
+systemctl start hostapd@wlan1.service
+
