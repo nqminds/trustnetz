a4bf4b2695627d98da3f4f17b2d3958e70b2a487
Alexandru Mereacre <mereacre@gmail.com>
2023-12-05 22:20:08 +0000

    feat: added installer for brsi gateway

---
+++ brski-server/installer/payload/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IF0="wlan0"
+IF1="wlan1"
+
+ip addr add 192.168.16.1/24 dev $IF0
+ip addr add 192.168.17.1/24 dev $IF1
+
+systemctl start hostapd@$IF0.service
+systemctl start hostapd@$IF1.service
+
+sudo systemctl restart dnsmasq@$IF0.service
+sudo systemctl restart dnsmasq@$IF1.service
+
+
