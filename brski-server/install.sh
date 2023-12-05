b7e1739868e708650fcafa74d7b7726ace8ae9d5
Alexandru Mereacre <mereacre@gmail.com>
2023-12-05 21:21:07 +0000

    feat: configured dnsmasq instalation

--- brski-server/install.sh
+++ brski-server/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-apt install hostapd dnsmasq iptables
+apt install hostapd dnsmasq iptables dhcpcd5
+
+cp dhcpcd.conf /etc
 
 service hostapd stop
 service dnsmasq stop
@@ -15,4 +17,14 @@ systemctl start hostapd@wlan0.service
 cp wlan1.conf /etc/hostapd/wlan1.conf
 systemctl enable hostapd@wlan1.service 
 systemctl start hostapd@wlan1.service
+
+cp dnsmasq.wlan0 /etc/default
+cp dnsmasq.wlan1 /etc/default
+
+sudo systemctl restart dnsmasq@wlan0.service
+sudo systemctl restart dnsmasq@wlan1.service
+
+sudo systemctl enable dnsmasq@wlan0.service
+sudo systemctl enable dnsmasq@wlan1.service
+
 
