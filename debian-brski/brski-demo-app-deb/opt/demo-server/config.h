6a711ddc08cefbcc68731bd8d384f0fc5242a6ed
ionut-cmd <bostanionut28@gmail.com>
2023-12-15 13:23:29 +0000

    add: config.h

---
+++ debian-brski/brski-demo-app-deb/opt/demo-server/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#define CONFIG_FILE_PATH "/opt/demo-server/html/server.conf"
+#define HTML_BASE_PATH "/opt/demo-server/html"
+#define ONBOARDING_LOG_FILE_PATH "/opt/demo-server/html/onboarding_log_file.txt"
+#define OFFBOARDING_LOG_FILE_PATH "/opt/demo-server/html/offboarding_log_file.txt"
+#define ONBOARDING_SCRIPT_PATH "/opt/demo-server/bash-scripts/onboard.sh"
+#define OFFBOARDING_SCRIPT_PATH "/opt/demo-server/bash-scripts/offboard.sh"
+
+#endif 
