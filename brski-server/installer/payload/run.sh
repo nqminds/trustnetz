28525c39d8da7197cb2a725ff31300896d7d1c6a
Alexandru Mereacre <mereacre@gmail.com>
2024-01-15 22:28:48 +0000

    chore: updated IPs

--- brski-server/installer/payload/run.sh
+++ brski-server/installer/payload/run.sh
@@ -3,8 +3,11 @@
 IF0="wlan0"
 IF1="wlan1"
 
-ip addr add 192.168.16.1/24 dev $IF0
-ip addr add 192.168.17.1/24 dev $IF1
+IP0="192.168.16.1/24"
+IP1="192.168.17.1/24"
+
+ip addr add $IP0 dev $IF0
+ip addr add $IP1 dev $IF1
 
 systemctl start hostapd@$IF0.service
 systemctl start hostapd@$IF1.service
