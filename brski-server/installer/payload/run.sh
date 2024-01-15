a1f74ccd8791f93f70d10d1991154f8486c67205
Alexandru Mereacre <mereacre@gmail.com>
2024-01-15 22:22:24 +0000

    chore: fixed background run

--- brski-server/installer/payload/run.sh
+++ brski-server/installer/payload/run.sh
@@ -9,8 +9,9 @@ ip addr add 192.168.17.1/24 dev $IF1
 systemctl start hostapd@$IF0.service
 systemctl start hostapd@$IF1.service
 
-sudo systemctl restart dnsmasq@$IF0.service
-sudo systemctl restart dnsmasq@$IF1.service
+systemctl restart dnsmasq@$IF0.service
+systemctl restart dnsmasq@$IF1.service
 
 brski -c /etc/brski/config.ini masa -d &
-brski -c /etc/brski/config.ini registrar -d &
+brski -c /etc/brski/config.ini registrar -d
+
