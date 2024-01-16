2ec76cea240a88ab7e636db4bb364c4f8c907926
Alexandru Mereacre <mereacre@gmail.com>
2024-01-16 22:59:05 +0000

    feat: added offboarding

--- debian-brski/brski-demo-app-deb/opt/demo-server/bash-scripts/offboard.sh
+++ debian-brski/brski-demo-app-deb/opt/demo-server/bash-scripts/offboard.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 # offboard.sh
+
 echo "Offboarding IoT device..."
 
+
+file /opt/demo-server/certs/eap-tls-client.crt
+
+EAP_NAME=`openssl x509 -noout -issuer -in "/opt/demo-server/certs/eap-tls-client.crt" | sed -e 's/.*CN = \(.*\).*/\1/'`
+[[ -z "$EAP_NAME" ]] && { echo "Error: No EAP name found"; exit 1; }
+
 nmcli device disconnect wlan0
+
+echo "Removinf connection to $EAP_NAME ..."
+nmcli connection show $EAP_NAME > /dev/null
+
+if [ $? -eq 0 ]; then
+	nmcli connection delete id $EAP_NAME
+fi
+
+rm -f /opt/demo-server/certs/pinned-domain-ca.crt
+rm -f /opt/demo-server/certs/eap-tls-client.crt
+rm -f /opt/demo-server/certs/eap-tls-client.key
+
--- debian-brski/brski-demo-app-deb/opt/demo-server/bash-scripts/onboard.sh
+++ debian-brski/brski-demo-app-deb/opt/demo-server/bash-scripts/onboard.sh
@@ -21,17 +21,28 @@ echo "Connected to brski-open."
 
 sleep 2
 
-echo "Running brski sign command..."
-brski -c /etc/brski/config.ini sign -o "$CERTS_PATH/peer-client" -d
+echo "Running brski preq command..."
+brski -c /etc/brski/config.ini preq -o "$CERTS_PATH/pinned-domain-ca" -d
 
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-file $CERTS_PATH/peer-client.crt
+file $CERTS_PATH/pinned-domain-ca.crt
+
+
+echo "Running brski sign command..."
+brski -c /etc/brski/config.ini sign -o "$CERTS_PATH/eap-tls-client" -d
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+file $CERTS_PATH/eap-tls-client.key
+file $CERTS_PATH/eap-tls-client.crt
 
 echo "Got BRSKI signed certificates"
-EAP_NAME=`openssl x509 -noout -issuer -in "$CERTS_PATH/peer-client.crt" | sed -e 's/.*CN = \(.*\).*/\1/'`
+EAP_NAME=`openssl x509 -noout -issuer -in "$CERTS_PATH/eap-tls-client.crt" | sed -e 's/.*CN = \(.*\).*/\1/'`
 [[ -z "$EAP_NAME" ]] && { echo "Error: No EAP name found"; exit 1; }
 sleep 2
 
@@ -54,9 +65,9 @@ nmcli c add type wifi ifname wlan0 con-name $EAP_NAME \
       802-11-wireless-security.key-mgmt wpa-eap \
       802-1x.eap tls \
       802-1x.identity brski@pledge \
-      802-1x.ca-cert /etc/brski/registrar-tls-ca.crt \
-      802-1x.client-cert /opt/demo-server/certs/peer-client.crt \
-      802-1x.private-key /opt/demo-server/certs/peer-client.key \
+      802-1x.ca-cert /opt/demo-server/certs/pinned-domain-ca.crt \
+      802-1x.client-cert /opt/demo-server/certs/eap-tls-client.crt \
+      802-1x.private-key /opt/demo-server/certs/eap-tls-client.key \
       802-1x.private-key-password s3cr3t
 
 nmcli c up $EAP_NAME
