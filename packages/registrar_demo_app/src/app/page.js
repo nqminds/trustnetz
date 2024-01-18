c8de4a9ac2bf23ace5bcd32ff36cd3fc3dec12b1
Ash Setter <ash@nquiringminds.com>
2024-01-18 15:20:16 +0000

    retrieve data about selected device, manufacturer and device type and render them as tables

--- packages/registrar_demo_app/src/app/page.js
+++ packages/registrar_demo_app/src/app/page.js
@@ -1,5 +1,6 @@
 'use client';
 import React, {useState, useEffect} from 'react';
+import Table from '@/components/table';
 import './MyComponent.css';
 
 const ListItem = ({ leftContent, rightContent }) => (
@@ -11,7 +12,6 @@ const ListItem = ({ leftContent, rightContent }) => (
 
 const MyComponent = () => {
   const [vcLog, setVcLog] = useState([]);
-  const [deviceType, setDeviceTypeState] = useState('Raspberry Pi');
   const [deviceList, setDeviceList] = useState([]);
   const [deviceTypeList, setDeviceTypeList] = useState([]);
   const [manufacturerList, setManufacturerList] = useState([]);
@@ -20,18 +20,64 @@ const MyComponent = () => {
   const [selectedManufacturer, setSelectedManufacturer] = useState('');
   const [selectedDeviceType, setSelectedDeviceType] = useState('');
 
-  console.log(deviceList, deviceTypeList, manufacturerList);
+  const [selectedDeviceInfo, setSelectedDeviceInfo] = useState({});
+  const [selectedManufacturerInfo, setSelectedManufacturerInfo] = useState({});
+  const [selectedDeviceTypeInfo, setSelectedDeviceTypeInfo] = useState({});
 
   const fetchVcLog = async () => {
     try {
       const response = await fetch('http://localhost:3001/vc-logs');
       const vcLog = await response.json();
-      console.log(vcLog);
       setVcLog(vcLog.map(({log}) => log));
     } catch (error) {
       console.error('Error fetching VC Log:', error);
     }
   };
+
+  const fetchDeviceData = async (device) => {
+    const deviceToGetInfoFor = device ? device : selectedDevice;
+    console.log(`deviceToGetInfoFor: ${deviceToGetInfoFor}`)
+    try {
+      if (deviceToGetInfoFor) {
+        const selectedDeviceUrlEncoded = encodeURIComponent(deviceToGetInfoFor);
+        const response = await fetch(`http://localhost:3001/info/device/${selectedDeviceUrlEncoded}`);
+        const deviceInfo = await response.json();
+        setSelectedDeviceInfo(deviceInfo);
+      }
+    } catch (error) {
+      console.error('Error fetching VC Log:', error);
+    }
+  }
+
+  const fetchDeviceTypeData = async (deviceType) => {
+    const deviceTypeToGetInfoFor = deviceType ? deviceType : selectedDeviceType;
+    console.log(`deviceTypeToGetInfoFor: ${deviceTypeToGetInfoFor}`)
+    try {
+      if (deviceTypeToGetInfoFor) {
+        const selecteddeviceTypeUrlEncoded = encodeURIComponent(deviceTypeToGetInfoFor);
+        const response = await fetch(`http://localhost:3001/info/device-type/${selecteddeviceTypeUrlEncoded}`);
+        const deviceTypeInfo = await response.json();
+        setSelectedDeviceTypeInfo(deviceTypeInfo);
+      }
+    } catch (error) {
+      console.error('Error fetching VC Log:', error);
+    }
+  }
+
+  const fetchManufacturerData = async (manufacturer) => {
+    const manufacturerToGetInfoFor = manufacturer ? manufacturer : selectedManufacturer;
+    console.log(`manufacturerToGetInfoFor: ${manufacturerToGetInfoFor}`)
+    try {
+      if (manufacturerToGetInfoFor) {
+        const selectedManufacturerUrlEncoded = encodeURIComponent(manufacturerToGetInfoFor);
+        const response = await fetch(`http://localhost:3001/info/manufacturer/${selectedManufacturerUrlEncoded}`);
+        const manufacturerInfo = await response.json();
+        setSelectedManufacturerInfo(manufacturerInfo);
+      }
+    } catch (error) {
+      console.error('Error fetching VC Log:', error);
+    }
+  }
 
   useEffect(() => {
     // Fetch initial inputValue from an API route when the component mounts
@@ -225,7 +271,10 @@ const MyComponent = () => {
           Manufacturer Select:
           <select
             value={selectedManufacturer}
-            onChange={(e) => setSelectedManufacturer(e.target.value)}
+            onChange={(e) => {
+              setSelectedManufacturer(e.target.value);
+              fetchManufacturerData(e.target.value);
+            }}
           >
             <option value="">Select a manufacturer</option>
             {manufacturerList.map((option, index) => (
@@ -241,7 +290,10 @@ const MyComponent = () => {
           Device Select:
           <select
             value={selectedDevice}
-            onChange={(e) => setSelectedDevice(e.target.value)}
+            onChange={(e) => {
+              setSelectedDevice(e.target.value);
+              fetchDeviceData(e.target.value);
+            }}
           >
             <option value="">Select a device</option>
             {deviceList.map((option, index) => (
@@ -257,7 +309,10 @@ const MyComponent = () => {
           Device Type Select:
           <select
             value={selectedDeviceType}
-            onChange={(e) => setSelectedDeviceType(e.target.value)}
+            onChange={(e) => {
+              setSelectedDeviceType(e.target.value);
+              fetchDeviceTypeData(e.target.value);
+            }}
           >
             <option value="">Select a device type</option>
             {deviceTypeList.map((option, index) => (
@@ -275,7 +330,7 @@ const MyComponent = () => {
       {/* Vertical line */}
       <div className="vertical-line"></div>
 
-      {/* Right side with scrollable list */}
+      {/* Middle side with scrollable list */}
       <div className="list-container">
         <h3>List of submitted VC and Responses:</h3>
         <ul className="scrollable-list">
@@ -289,6 +344,28 @@ const MyComponent = () => {
             </li>
           ))}
         </ul>
+      </div>
+
+
+      {/* Vertical line */}
+      <div className="vertical-line"></div>
+
+      {/* Right side with information */}
+      <div className="info-container">
+      <div className="table-section">
+        <h2>Manufacturer</h2>
+        <Table data={selectedManufacturerInfo} />
+      </div>
+
+      <div className="table-section">
+        <h2>Device</h2>
+        <Table data={selectedDeviceInfo} />
+      </div>
+
+      <div className="table-section">
+        <h2>Device Type</h2>
+        <Table data={selectedDeviceTypeInfo} />
+      </div>
       </div>
     </div>
   );
---
+++ packages/registrar_demo_app/src/components/table.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Table = ({ data }) => {
+  if (!data || Object.keys(data).length === 0) {
+    return <p>No data available</p>;
+  }
+
+  const headers = Object.keys(data);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Key</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {headers.map((header) => (
+          <tr key={header}>
+            <td>{header}</td>
+            <td>{renderValue(data[header])}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const renderValue = (value) => {
+  // Check if the value is boolean
+  if (typeof value === 'boolean') {
+    return value ? 'True' : 'False';
+  }
+
+  // Handle other types if needed
+  return value;
+};
+
+export default Table;
