dd7825cb1bc8a650b0bedad129c4b71a9570bca3
antmcc49 <antmcc49@gmail.com>
2024-01-23 12:04:25 +0000

    restructure bash scripts

--- packages/docusaurus/scripts/import-external.sh
+++ packages/docusaurus/scripts/import-external.sh
@@ -5,15 +5,15 @@ cd "docs/build5-docs"
 wget "https://raw.githubusercontent.com/nqminds/nist-brski/main/brski-server/README.md" -O "25-BRSKI demonstrator/20-demo-setup.md"
 
 
-cp "../../../schemas/README.md" "60-CA Demo/10-schemas-setup.md"
+cp "../../../schemas/README.md" "60-CA Demo/30-schemas-setup.md"
 cp ../../../schemas/*.png "60-CA Demo/"
 
-cp "../../../nist_policy/README.md" "60-CA Demo/20-nist_policy.md"
+cp "../../../nist_policy/README.md" "60-CA Demo/40-nist_policy.md"
 
-cp "../../../nist_vc_rest_server/README.md" "60-CA Demo/30-nist_vc_rest_server.md"
+cp "../../../nist_vc_rest_server/README.md" "60-CA Demo/50-nist_vc_rest_server.md"
 cp ../../../nist_vc_rest_server/*.png "60-CA Demo/"
 
-cp "../../../nist_registrar_server/README.md" "60-CA Demo//40-nist_registrar_server.md"
+cp "../../../nist_registrar_server/README.md" "60-CA Demo/60-nist_registrar_server.md"
 cp ../../../nist_registrar_server/*.png "60-CA Demo/"
 
 
