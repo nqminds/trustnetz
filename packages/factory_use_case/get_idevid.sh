2bc0c1ff7c0dab82173afbb709340aed9d5ffb84
jr7g19 <jr7g19@soton.ac.uk>
2024-02-16 12:00:27 +0000

    Added client tpm

--- packages/factory_use_case/get_idevid.sh
+++ packages/factory_use_case/get_idevid.sh
@@ -1,2 +1,1 @@
-openssl genrsa -out client.key 2048
-openssl req -new -key client.key -subj '/CN=client' | curl --cacert mcr.crt https://89.21.226.142:7000 --data-binary @- > idevid.crt
+openssl req -new -provider tpm2 -key handle:0x81000001 -subj '/CN=client' | curl --cacert mcr.crt https://89.21.226.142:7000 --data-binary @- > idevid.crt
