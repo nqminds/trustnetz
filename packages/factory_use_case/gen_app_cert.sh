b7fc8d3c56a4795420af2641e56983865a74f1e5
jr7g19 <jr7g19@soton.ac.uk>
2024-02-19 15:08:48 +0000

    Moved to ecc keys

---
+++ packages/factory_use_case/gen_app_cert.sh
@@ -0,0 +1,1 @@
+openssl req -x509 -sha256 -nodes -days 365 -subj "/CN=MCR" -newkey ec:<(openssl ecparam -name prime256v1) -keyout mcr.key -out mcr.crt -addext "subjectAltName=IP:89.21.226.142"
--- packages/factory_use_case/gen_keys.sh
+++
@@ -1,2 +0,0 @@
-openssl genrsa -out mcr.key 2048
-openssl req -x509 -new -nodes -key mcr.key -sha256 -days 365 -out mcr.crt -subj "/CN=MCR" -addext "subjectAltName=IP:89.21.226.142"
