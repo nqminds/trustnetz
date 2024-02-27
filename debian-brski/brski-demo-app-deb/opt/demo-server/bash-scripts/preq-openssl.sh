d5e7ff8e66edaf50612585aaf78c50dbe06d0334
Alexandru Mereacre <mereacre@gmail.com>
2024-02-27 23:00:21 +0000

    feat: added addr

--- debian-brski/brski-demo-app-deb/opt/demo-server/bash-scripts/preq-openssl.sh
+++ debian-brski/brski-demo-app-deb/opt/demo-server/bash-scripts/preq-openssl.sh
@@ -1,12 +1,14 @@
 BRSKI="/home/alexandru/projects/brski/build/linux/src/brski/brski"
 CONFIG="/home/alexandru/projects/brski/build/linux/tests/brski/test-config.ini"
 REGISTRAR_TLS_PATH="/tmp/registrar_tls.crt"
+REGISTRAR_ADDRESS="127.0.0.1"
+REGISTRAR_PORT="12345"
 
 VOUCHER_REQUEST=`${BRSKI} -c ${CONFIG} epvr`
 
-HTTP_REQUEST="POST /.well-known/brski/requestvoucher HTTP/1.1\r\nHost: 127.0.0.1\r\nContent-Type: application/voucher-cms+json\r\nContent-Length: ${#VOUCHER_REQUEST}\r\n\r\n${VOUCHER_REQUEST}"
+HTTP_REQUEST="POST /.well-known/brski/requestvoucher HTTP/1.1\r\nHost: ${REGISTRAR_ADDRESS}\r\nContent-Type: application/voucher-cms+json\r\nContent-Length: ${#VOUCHER_REQUEST}\r\n\r\n${VOUCHER_REQUEST}"
 
-openssl s_client -connect 127.0.0.1:12345 -tls1_3 -showcerts 2>/dev/null | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' > ${REGISTRAR_TLS_PATH}
+openssl s_client -connect ${REGISTRAR_ADDRESS}:${REGISTRAR_PORT} -tls1_3 -showcerts 2>/dev/null | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' > ${REGISTRAR_TLS_PATH}
 
 MASA_PVOUCHER_CMS=`echo -e "$HTTP_REQUEST" | openssl s_client -quiet -connect 127.0.0.1:12345 -cert idevid.crt -key idevid.key -tls1_3 2>/dev/null | sed -n '/^$/{g;D;}; N; $p;' | sed -r '/^\s*$/d'`
 
