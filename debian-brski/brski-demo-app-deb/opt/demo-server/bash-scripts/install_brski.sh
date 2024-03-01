8291aa3c75feeda0f05ebd41460128a952b791f8
ionut-cmd <bostanionut28@gmail.com>
2024-03-01 16:06:10 +0000

    update brski version

--- debian-brski/brski-demo-app-deb/opt/demo-server/bash-scripts/install_brski.sh
+++ debian-brski/brski-demo-app-deb/opt/demo-server/bash-scripts/install_brski.sh
@@ -3,23 +3,23 @@
 echo "Installing brski package..."
 
 # Download brski
-wget -O /tmp/brski_0.2.3_arm64.deb https://github.com/nqminds/brski/releases/download/0.2.3/brski_0.2.3_arm64.deb
+wget -O /tmp/brski_0.2.6_arm64.deb https://github.com/nqminds/brski/releases/download/0.2.6/brski_0.2.6_arm64.deb
 if [ $? -ne 0 ]; then
     echo "Failed to download brski. Please check your internet connection."
     exit 1
 fi
 
 # Install brski
-dpkg -i /tmp/brski_0.2.3_arm64.deb
+dpkg -i /tmp/brski_0.2.6_arm64.deb
 if [ $? -ne 0 ]; then
     echo "Failed to install brski. Please check for any dpkg errors."
     exit 1
 fi
 
 # Copy certificates (if necessary)
-cp /opt/demo-server/certs/* /etc/brski
+cp -r /opt/demo-server/certs/* /etc/brski
 
 # Clean up
-rm /tmp/brski_0.2.3_arm64.deb
+rm /tmp/brski_0.2.6_arm64.deb
 
 echo "brski installation completed."
