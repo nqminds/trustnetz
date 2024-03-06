1b1b4097905e74fc399fbaa069f1a18f009253fb
jr7g19 <jr7g19@soton.ac.uk>
2024-03-06 12:54:12 +0000

    Fixed content length

--- packages/app_onboarding/ping.sh
+++ packages/app_onboarding/ping.sh
@@ -1,3 +1,4 @@
-while echo -e "POST / HTTP/1.1\r\nContent-Length: 47\r\n\r\nDate: $(date), Temp: $(cat /sys/class/thermal/thermal_zone0/temp)" | sudo openssl s_client -provider tpm2 -provider default -CAfile app.crt -cert idevid.crt -key handle:0x81000001 89.21.226.142:4001;
-  do sleep 60;
-done
+content="Date: $(date), Temp: $(cat /sys/class/thermal/thermal_zone0/temp)"
+length=${#content}
+echo -e "POST / HTTP/1.1\r\nContent-Length: ${length}\r\n\r\n${content}" |
+sudo openssl s_client -provider tpm2 -provider default -CAfile app.crt -cert idevid.crt -key handle:0x81000001 89.21.226.142:4001
