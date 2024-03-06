1b7116fcb0455399822c2c53f607382e85867832
ionut-cmd <bostanionut28@gmail.com>
2024-03-06 13:30:57 +0000

    changed registrar path

--- debian-brski/brski-demo-app-deb/opt/demo-server/bash-scripts/onboard.sh
+++ debian-brski/brski-demo-app-deb/opt/demo-server/bash-scripts/onboard.sh
@@ -48,8 +48,8 @@ sleep 2
 
 echo "Running brski preq command..."
 # Uncoment bellow for using brski command
-brski -c /etc/brski/config.ini preq -p $PORT -a $IP -o "$CERTS_PATH/pinned-domain-ca" -d
-# bash /opt/demo-server/bash-scripts/preq-openssl.sh "$IP" "$PORT" "$CERTS_PATH"
+# brski -c /etc/brski/config.ini preq -p $PORT -a $IP -o "$CERTS_PATH/pinned-domain-ca" -d
+bash /opt/demo-server/bash-scripts/preq-openssl.sh "$IP" "$PORT" "$CERTS_PATH"
 if [ $? -ne 0 ]; then
     exit 1
 fi
@@ -58,8 +58,9 @@ file $CERTS_PATH/pinned-domain-ca.crt
 
 
 echo "Running brski sign command..."
-brski -c /etc/brski/config.ini sign -p $PORT -a $IP -o "$CERTS_PATH/eap-tls-client" -d
-
+# Uncoment bellow for using brski command
+# brski -c /etc/brski/config.ini sign -p $PORT -a $IP -o "$CERTS_PATH/eap-tls-client" -d
+bash /opt/demo-server/bash-scripts/sign_request.sh "$IP" "$PORT" "$CERTS_PATH"
 if [ $? -ne 0 ]; then
     exit 1
 fi
