bdd746363ddb183bd991cbb5d0cd3a7bde93b8d6
jr7g19 <jr7g19@soton.ac.uk>
2024-06-14 15:13:25 +0100

    Moved to registrar

--- packages/app_onboarding/ping.sh
+++ packages/app_onboarding/ping.sh
@@ -1,4 +1,4 @@
 content="Date: $(date), Temp: $(cat /sys/class/thermal/thermal_zone0/temp)"
 length=${#content}
 echo -e "POST / HTTP/1.1\r\nContent-Length: ${length}\r\n\r\n${content}" |
-sudo openssl s_client -provider tpm2 -provider default -CAfile app.crt -cert idevid.crt -key handle:0x81000001 89.21.226.142:4001
+sudo openssl s_client -provider tpm2 -provider default -CAfile app.crt -cert idevid.crt -key handle:0x81000001 192.168.50.112:4001
