9a52546716e9732ec9a1d0671ca05d59ca4b5df3
A9-dev <henrypearson422@gmail.com>
2024-08-21 09:43:44 +0100

    feat: improve page layout for device type and manufacturer pages

--- packages/cahn_demo/client/src/app/device-type/[device_type_id]/page.js
+++ packages/cahn_demo/client/src/app/device-type/[device_type_id]/page.js
@@ -1,9 +1,27 @@
-import { Typography, Container } from "@mui/material";
+import { Typography, Box, Paper } from "@mui/material";
 
 export default function Page({ params }) {
   return (
-    <Container>
-      <Typography>Device type id: {params.device_type_id}</Typography>
-    </Container>
+    <Box>
+      <Typography
+        variant="h1"
+        sx={{
+          textAlign: "center",
+          color: "primary.main",
+          m: { xs: 1, sm: 3 },
+        }}
+      >
+        Device Type Information
+      </Typography>
+      <Paper
+        sx={{
+          m: { xs: 1, sm: 3 },
+          p: { xs: 2, sm: 3 },
+          minWidth: 300,
+        }}
+      >
+        <Typography>Device type id: {params.device_type_id}</Typography>
+      </Paper>
+    </Box>
   );
 }
--- packages/cahn_demo/client/src/app/manufacturer/[manufacturer_id]/page.js
+++ packages/cahn_demo/client/src/app/manufacturer/[manufacturer_id]/page.js
@@ -1,9 +1,27 @@
-import { Typography, Container } from "@mui/material";
+import { Typography, Box, Paper } from "@mui/material";
 
 export default function Page({ params }) {
   return (
-    <Container>
-      <Typography>Manufacturer id: {params.manufacturer_id}</Typography>
-    </Container>
+    <Box>
+      <Typography
+        variant="h1"
+        sx={{
+          textAlign: "center",
+          color: "primary.main",
+          m: { xs: 1, sm: 3 },
+        }}
+      >
+        Manufacturer Information
+      </Typography>
+      <Paper
+        sx={{
+          m: { xs: 1, sm: 3 },
+          p: { xs: 2, sm: 3 },
+          minWidth: 300,
+        }}
+      >
+        <Typography>Manufacturer id: {params.manufacturer_id}</Typography>
+      </Paper>
+    </Box>
   );
 }
