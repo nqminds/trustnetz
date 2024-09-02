28e81df58a9a92d4fd3e22d3b0f98d41e4e176d3
A9-dev <henrypearson422@gmail.com>
2024-09-02 11:48:47 +0100

    feat(DeviceTypeInfoTable): update column header to 'Has Trust From Users'

--- packages/cahn_demo/client/src/app/components/DeviceTypeInfoTable.js
+++ packages/cahn_demo/client/src/app/components/DeviceTypeInfoTable.js
@@ -130,7 +130,7 @@ export default function DeviceTypeInfoTable({ deviceTypeData }) {
                         "&:last-child td, &:last-child th": { border: 0 },
                       }}
                     >
-                      <StyledTableCell>Has Trust</StyledTableCell>
+                      <StyledTableCell>Has Trust From Users</StyledTableCell>
                       <StyledTableCell align="right">
                         <Chip
                           icon={
